18765b24d4a89c2a144296ab30dc68614080cce3
JuliaCarew <crewjulia3@gmail.com>
2024-10-09 12:30:09 -0300

    more pin adjustments

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class BallController : MonoBehaviour
 {
     public GameObject ball;
     public Rigidbody rb_ball;
     public GameObject aimGuide;
-    public TextMeshProUGUI countText;
+    public TextMeshProUGUI ScoreSheet;
     public GameObject winTextObject;
     public GameObject holdKeyObject;
+    public Text accuracyText;
     public bool ballStopped;
     public float speed = 0; 
     private int score;
+    private int throws;
     private float ballVeloityMagnitude;
     public bool heldKey;
     public float holdDuration = 0;
+    private float accuracy = 55;
 
     private Vector3 scaleChange, positionChange;
 
@@ -26,6 +31,7 @@ public class BallController : MonoBehaviour
     {
         ballStopped = true;
         score = 0;
+        throws = 0;
         SetScoreText();
         winTextObject.SetActive(false);
         holdKeyObject.SetActive(false); //somehow always displaying??
@@ -33,7 +39,6 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ballVeloityMagnitude = rb_ball.velocity.magnitude;
         ballShoot();
     }
     private void OnTriggerEnter(Collider other)
@@ -49,25 +54,27 @@ public class BallController : MonoBehaviour
         if (Input.GetKeyDown("space") && holdDuration < 5f)
         {
             ballStopped = false;
-            //rb_ball.AddForce(aimGuide.transform.forward * 25, ForceMode.VelocityChange);
             rb_ball.AddForce(new Vector3(0, 0, 30), ForceMode.Impulse);
             heldKey = false;
+            throws++;
         }
         //set up hold shoot - display ui w it
         else 
         {
             ballStopped = false;
             heldKey = true;
-            holdKeyObject.SetActive(true);
+            //holdKeyObject.SetActive(true);
             //Add multiplicative force to rb_ball * holdDuration ? 
         }
     }
     void SetScoreText()
     {
-        countText.text = "Count: " + score.ToString();
-        if (score >= 8)
+        //ScoreSheet.text = "TEST" + score.ToString();
+        if (score > 10)
         {
             winTextObject.SetActive(true);
+            accuracy = (score / 300) * 100;
+            accuracyText.text = accuracy.ToString();
         }
     }
     public void SetBallToStart() //not triggering
